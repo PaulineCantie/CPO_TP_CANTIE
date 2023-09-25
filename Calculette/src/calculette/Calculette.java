68af102f35c89b1bfb1f6869b17da92226863971
canti <canti@LAPTOP-MPIM17SR>
2023-09-25 22:16:04 +0200

    CANTIE Pauline, TP0 Exercice 1 Exercice 2 Exercice 3

--- Calculette/src/calculette/Calculette.java
+++ Calculette/src/calculette/Calculette.java
@@ -17,21 +17,37 @@ public class Calculette {
      * @param args the command line arguments
      */
     public static String main(String[] args) {
-       System.out.println("Please enter the operator: \n 1) add \n 2)substract \n 3) multiply \n 4) divide \n 5) modulo");
-       
-       String valeur1;
-       String valeur2;
-       Scanner sc;
-       sc = new Scanner(System.in);
-       System.out.println("Quelle est la premiere valeur ?");
-       valeur1 = sc.nextLine();  
-       System.out.println("Quelle est la deuxieme valeur ?");
-       valeur2 = sc.nextLine();  
-       
-       if valeur1<valeur2;
-       return("Le minimun est :" valeur1);
-       
-       
-    }
+    Scanner sc= new Scanner (System.in);
     
-}
+    int operateur;
+    System.out.println("Please enter the operator: \n 1) add \n 2)substract \n 3) multiply \n 4) divide \n 5) modulo");
+    operateur=sc.nextInt();
+    
+    int nb1;
+    System.out.println("Entrer un nombre1");
+    nb1 = sc.nextInt();
+    
+    int nb2;
+    System.out.println("Entrer un nombre2");
+    nb2 = sc.nextInt();
+    
+    int nb;
+    if (operateur ==1){
+        nb = nb1 + nb2;
+        System.out.println("nombre1 + nombre2 = "+nb);
+    }
+
+    if (operateur ==3){
+        nb = nb1 * nb2;
+        System.out.println("nombre1 * nombre2 = "+nb);
+    }
+
+    if (operateur ==4){
+        nb = nb1 / nb2;
+        System.out.println("nombre1 / nombre2 = "+nb);
+    }
+
+    if (operateur ==5){
+        nb = nb1 % nb2;
+        System.out.println("nombre1 modulo nombre2 = "+nb);
+    }
