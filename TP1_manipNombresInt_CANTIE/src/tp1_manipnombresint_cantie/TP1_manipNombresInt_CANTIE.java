1f09192da26202d3d7de203ac105fee54225e6fc
canti <canti@LAPTOP-MPIM17SR>
2023-09-26 16:25:33 +0200

    CANTIE Pauline, TP1 Exercice 1

---
+++ TP1_manipNombresInt_CANTIE/src/tp1_manipnombresint_cantie/TP1_manipNombresInt_CANTIE.java
@@ -0,0 +1,20 @@
+/*
+ * CANTIE Pauline TDC
+ * TP1
+ * Septembre 2023
+ */
+package tp1_manipnombresint_cantie;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author canti
+ */
+public class TP1_manipNombresInt_CANTIE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
