66c13824ad34f50d08288a743f0c68494180c1f5
canti <canti@LAPTOP-MPIM17SR>
2023-09-26 16:26:25 +0200

    CANTIE Pauline, TP1 Exercice 2

---
+++ TP1_manipNombresInt_CANTIE/2 Convertisseur de temperature/src/pkg2/convertisseur/de/temperature/ConvertisseurDeTemperature.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2.convertisseur.de.temperature;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author canti
+ */
+public class ConvertisseurDeTemperature {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner sc;
+        sc= new Scanner(System.in);
+        
+    double ValeurR;
+    System.out.println("Entrer une valeur reelle :");
+    ValeurR = sc.nextDouble();
+    }
+    
+}
