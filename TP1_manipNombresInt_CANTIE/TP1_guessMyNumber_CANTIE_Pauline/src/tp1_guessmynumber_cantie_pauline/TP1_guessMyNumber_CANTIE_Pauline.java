475268afb125c82929bceb208129513fef8fa203
canti <canti@LAPTOP-MPIM17SR>
2023-10-04 15:29:51 +0200

    CANTIE Pauline, TP1 Guess My Number

---
+++ TP1_manipNombresInt_CANTIE/TP1_guessMyNumber_CANTIE_Pauline/src/tp1_guessmynumber_cantie_pauline/TP1_guessMyNumber_CANTIE_Pauline.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_cantie_pauline;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *TP1 exercice 3
+ * CANTIE Pauline
+ * 4 octobre 2023
+ * @author canti
+ */
+public class TP1_guessMyNumber_CANTIE_Pauline {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner sc= new Scanner (System.in);
+
+        
+        Random generateurAleat=new Random();
+
+        int n = generateurAleat.nextInt(100);
+
+        int nb ;
+        System.out.println("Choissisez un nombre entre 0 et 100");
+        nb = sc.nextInt();
+        
+        if nb == n;
+                
+        
+    }
+
+    }
+
+ 
+
