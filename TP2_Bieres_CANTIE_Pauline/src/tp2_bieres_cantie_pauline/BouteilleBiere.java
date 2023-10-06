16bdb38e714f805309f4ec0a1bffa59f6fd280f1
canti <canti@LAPTOP-MPIM17SR>
2023-10-06 18:47:17 +0200

    CANTIE Pauline, TP2 1 Création d’une première classe et de quelques objets associés

---
+++ TP2_Bieres_CANTIE_Pauline/src/tp2_bieres_cantie_pauline/BouteilleBiere.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_cantie_pauline;
+
+/**
+ *
+ * @author canti
+ */
+public class BouteilleBiere {
+BouteilleBiere.nom();
+
+}
