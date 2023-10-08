ac4e08de39980c99c00d89e224a98e13f00e40e2
canti <canti@LAPTOP-MPIM17SR>
2023-10-08 21:47:01 +0200

    CANTIE Pauline, TP2 GuessMyNumber TP1

--- TP1_manipNombresInt_CANTIE/TP1_guessMyNumber_CANTIE_Pauline/src/tp1_guessmynumber_cantie_pauline/TP1_guessMyNumber_CANTIE_Pauline.java
+++ TP1_manipNombresInt_CANTIE/TP1_guessMyNumber_CANTIE_Pauline/src/tp1_guessmynumber_cantie_pauline/TP1_guessMyNumber_CANTIE_Pauline.java
@@ -20,22 +20,91 @@ public class TP1_guessMyNumber_CANTIE_Pauline {
      */
     public static void main(String[] args) {
     Scanner sc= new Scanner (System.in);
-
+        int choixmode;
+        System.out.println("Entrez un mode de jeu: \n1 pour facile \n2 pour moyen \n3 pour difficile");
+        choixmode=sc.nextInt();
         
-        Random generateurAleat=new Random();
-
-        int n = generateurAleat.nextInt(100);
-
-        int nb ;
-        System.out.println("Choissisez un nombre entre 0 et 100");
-        nb = sc.nextInt();
+        int nombre;
+        Random alea = new Random();
+        int n1 = alea.nextInt(100);
+              System.out.println("Entrez un nombre entre 0 et 100");
+        nombre = sc.nextInt();
+        int result;
         
-        if nb == n;
-                
+        if (choixmode==1){
+            result=modefacile(nombre,n1);
+            System.out.println("Vous avez gagne en " + result + " coups");
+        }
         
+        else if (choixmode==3){
+            result=modedifficile(nombre,n1);
+            if (result!=5){
+                System.out.println("Vous avez gagne en "+result+" coups");
+            } else if(result==5){
+                System.out.println("Perdu! vous avez utilise les 5 coups");
+            }
+        }   
+        else if (choixmode==2){
+            result=modemoyen(nombre,n1);
+            System.out.println("Vous avez gagne en " + result + " coups");
+
+        }
+    
+    
+
+    public static int modefacile(int nbaleatoire, int nbchoisi) {
+        Scanner sc = new Scanner(System.in);
+        int nbessaie = 1;
+        while (nbchoisi != nbaleatoire) {
+            if (nbchoisi < nbaleatoire) {
+                System.out.println(" nombre trop grand, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            } else if (nbchoisi > nbaleatoire) {
+                System.out.println("nombre trop petit, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            }
+        }
+        return nbessaie;
     }
-
+    
+    public static int modedifficile(int nbaleatoire, int nbchoisi) {
+        Scanner sc = new Scanner(System.in);
+        int nbessaie = 1;
+        while (nbchoisi != nbaleatoire && nbessaie!=5) {
+            if (nbchoisi < nbaleatoire) {
+                System.out.println("nombre trop grand, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            } else if (nbchoisi > nbaleatoire) {
+                System.out.println("nombre trop petit, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            }
+        } if (nbessaie==5){
+            return 5;
+        }
+        return nbessaie;
     }
+    
+    
+    public static int modemoyen(int nbaleatoire, int nbchoisi) {
+        Scanner sc = new Scanner(System.in);
+        int nbessaie = 1;
+        while (nbchoisi != nbaleatoire) {
+            if (nbchoisi-nbaleatoire>20) {
+                System.out.println(" nombre beaucoup trop eloigne, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            } else if (nbchoisi - nbaleatoire< 20) {
+                System.out.println("nombre tres proche, choisir un autre nombre:");
+                nbaleatoire = sc.nextInt();
+                nbessaie += 1;
+            }
+        }
+        return nbessaie;
+    }
+}
 
- 
 
