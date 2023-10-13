cae73170b915a46140653979d1a9591add6936f6
canti <canti@LAPTOP-MPIM17SR>
2023-10-13 15:08:44 +0200

    CANTIE Pauline, TP2 Exercice 1

--- TP2_Bieres_CANTIE_Pauline/src/tp2_bieres_cantie_pauline/TP2_Bieres_CANTIE_Pauline.java
+++ TP2_Bieres_CANTIE_Pauline/src/tp2_bieres_cantie_pauline/TP2_Bieres_CANTIE_Pauline.java
@@ -16,25 +16,26 @@ public class TP2_Bieres_CANTIE_Pauline {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-BouteilleBiere uneBiere = new BouteilleBiere() ;   
-uneBiere.nom = "Cuvee des trolls";
-uneBiere.degreAlcool = 7.0 ;
-uneBiere.brasserie= "Dubuisson";
-uneBiere.ouverte=false;        
-uneBiere.lireEtiquette();
-BouteilleBiere deuxBiere = new BouteilleBiere() ;   
-deuxBiere.nom="Leffe";
-deuxBiere.degreAlcool=6.6;
-deuxBiere.brasserie="Abbaye de Leffe";
-deuxBiere.lireEtiquette();
-
-BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
-public BouteilleBiere(String unNom, float unDegre, String 
-uneBrasserie) {
- nom = "Leffe";
- degreAlcool = 6.6;
- brasserie = "Abbaye de Leffe";
- ouverte = false;
-        }
+//BouteilleBiere uneBiere = new BouteilleBiere() ;   
+//uneBiere.nom = "Cuvee des trolls";
+//uneBiere.degreAlcool = 7.0 ;
+//uneBiere.brasserie= "Dubuisson";
+//uneBiere.ouverte=false;        
+//uneBiere.lireEtiquette();
+//BouteilleBiere deuxBiere = new BouteilleBiere() ;   
+//deuxBiere.nom="Leffe";
+//deuxBiere.degreAlcool=6.6;
+//deuxBiere.brasserie="Abbaye de Leffe";
+//deuxBiere.lireEtiquette();
+//    }
+    
+BouteilleBiere B3 = new BouteilleBiere("Delirium Tremens", (float) 8.5,"Brasserie Huyghe") ;
+    B3.Décapsuler(); //methode  decapsuler 
+        BouteilleBiere B2 = new BouteilleBiere("Rince cochon", (float) 8.5,"Brasserie Rince Cochon") ;
+        BouteilleBiere B1 = new BouteilleBiere("BMB Blanche", (float) 5.6,"Brasserie Mont Blanc") ;
+    B1.Décapsuler();//methode  decapsuler
+        
+    System.out.println(B1 + "\n" + B2 + "\n" + B3); //permet de lire avec toString()
     }
+ 
 }
