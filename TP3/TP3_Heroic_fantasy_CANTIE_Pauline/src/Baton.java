fea265231336bfc9670760c9aad9537e789bd2fb
canti <canti@LAPTOP-MPIM17SR>
2023-10-18 15:27:35 +0200

    CANTIE Pauline, TP3 Exercice 1

---
+++ TP3/TP3_Heroic_fantasy_CANTIE_Pauline/src/Baton.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author canti
+ */
+public class Baton extends Arme {
+    int ageA;
+
+    public Baton(String nom, int attaque, int age) {
+        super(nom, attaque);
+        ageA=age;
+        
+    }
+}
