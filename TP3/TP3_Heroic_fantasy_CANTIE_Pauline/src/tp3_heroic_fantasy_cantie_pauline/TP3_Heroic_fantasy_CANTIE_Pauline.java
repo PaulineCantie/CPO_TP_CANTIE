63b80e77f4f0994cb4c84c719cf4fe2ac7ca712c
canti <canti@LAPTOP-MPIM17SR>
2023-10-18 15:27:58 +0200

    CANTIE Pauline, TP3 Exercice 1

---
+++ TP3/TP3_Heroic_fantasy_CANTIE_Pauline/src/tp3_heroic_fantasy_cantie_pauline/TP3_Heroic_fantasy_CANTIE_Pauline.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_cantie_pauline;
+
+/**
+ *
+ * @author canti
+ */
+public class TP3_Heroic_fantasy_CANTIE_Pauline {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+class abstract Arme{
+    String nom;
+    String niveauAttaque;
+    String NOM = nom.toString();
+    String NIVEAU = niveauAttaque.toString();
+    
+}
+}
+    
+}
