b998cf66503b7d79577494e3f1bd9b2b34229365
canti <canti@LAPTOP-MPIM17SR>
2023-10-18 15:26:41 +0200

    CANTIE Pauline, TP3 Exercice 1

---
+++ TP3/TP3_Heroic_fantasy_CANTIE_Pauline/src/Epee.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author canti
+ */
+public class Epee extends Arme {
+    int indiceFinesse;
+
+    public Epee(String nom, int attaque, int indice) {
+        super(nom, attaque);
+        indiceFinesse=indice;
+    }
+    
+    
+}
